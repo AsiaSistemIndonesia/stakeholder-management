b1743e5e5c8bd5fc62163a651d037d65a18aba19
febri <soerjahartono22@gmail.com>
2026-07-22 15:27:41 +0700

    chore: hide searchbar

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -23,14 +23,14 @@ export function Header({ title, subtitle }: HeaderProps) {
 
         <div className="flex items-center gap-4">
           {/* Search */}
-          <div className="relative hidden md:block">
+          {/* <div className="relative hidden md:block">
             <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
             <Input
               type="search"
               placeholder="Cari stakeholder, organisasi..."
               className="w-64 pl-9 bg-secondary border-border"
             />
-          </div>
+          </div> */}
 
           {/* Theme Toggle */}
           <ThemeToggle />
